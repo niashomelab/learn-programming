c4777f54d4664ffb808bcc06c9ceac9c083f2f2a
Nghia <niashomelab@gmail.com>
2018-08-23 18:20:43 +0200

    add more comments

--- python/chap15_wargame.py
+++ python/chap15_wargame.py
@@ -6,12 +6,15 @@ from random import shuffle
 
 
 class Card:
-    suits = ["spades",
-             "hearts",
-             "diamonds",
-             "clubs"]
+    """
+    spades > hearts > diamonds > clubs
+    """
+    suits = ["SPADES",
+             "HEARTS",
+             "DIAMONDS",
+             "CLUBS"]
 
-    values = [None, None,"2", "3",
+    values = [None, None,"2", "3", # 2 "None" so the string "2" in the list is at index 2
               "4", "5", "6", "7",
               "8", "9", "10",
               "Jack", "Queen",
@@ -23,6 +26,7 @@ class Card:
         self.suit = s
 
     def __lt__(self, c2):
+        # less than; card 2
         if self.value < c2.value:
             return True
         if self.value == c2.value:
@@ -33,6 +37,7 @@ class Card:
         return False
 
     def __gt__(self, c2):
+        # greater than; card 2
         if self.value > c2.value:
             return True
         if self.value == c2.value:
@@ -43,61 +48,65 @@ class Card:
         return False
 
     def __repr__(self):
+        # print card i.e. 3 of diamonds
         v = self.values[self.value] \
             + " of " + \
             self.suits[self.suit]
         return v
 
-
+# from random import shuffle for class Deck
 class Deck:
     def __init__(self):
-        self.cards = []
-        for i in range(2, 15):
-            for j in range(4):
+        # create 52 cards - one card for every suit+value combi; 52 = ((15-2)+1)*4
+        self.cards = [] # cards list
+        for i in range(2, 15): # 2..10 + jack + queen + king + ace = 13; last value = 14, begin with 2; 
+            for j in range(4): # 0,1,2,3 = 4 suits
                 self.cards.append(Card(i,j))
-        shuffle(self.cards)
+        shuffle(self.cards) # randomly rearranges the items in the list "cards", mimickking the shuffling of a deck of cards
 
     def rm_card(self):
-        if len(self.cards) == 0:
+        # remove card from the list "cards"
+        if len(self.cards) == 0: # empty list => none
             return
-        return self.cards.pop()
+        return self.cards.pop() # remove the drawed card
 
 
 class Player:
     def __init__(self, name):
-        self.wins = 0
-        self.card = None
-        self.name = name
+        self.wins = 0 # how many rounds player has won
+        self.card = None # card player is currently holding
+        self.name = name # player name
 
 
 class Game:
     def __init__(self):
         name1 = input("p1 name: ")
         name2 = input("p2 name: ")
-        self.deck = Deck()
+        self.deck = Deck() # create deck
         self.p1 = Player(name1)
         self.p2 = Player(name2)
 
     def wins(self, winner):
+        # win a round
         w = "'{}' wins this round"
         w = w.format(winner)
         print(w)
 
     def draw(self, p1n, p1c, p2n, p2c):
-        d = "'{}' drew {} ; '{}' drew {}"
-        d = d.format(p1n,
-                     p1c,
-                     p2n,
-                     p2c)
-        print(d)
+        d = "'{}' drew {} ; '{}' drew {}" # draw
+        d = d.format(p1n, # player 1's name
+                     p1c, # player 1's card
+                     p2n, # player 2's name
+                     p2c) # player 2's card
+        print(d) # print draw list
 
     def play_game(self):
         cards = self.deck.cards
         print("\nbeginning War!")
         while len(cards) >= 2:
-            m = "\nq to quit. Any key to play: "
+            m = "\nq to quit. Any key to play: " # m = message
             response = input(m)
-            if response == 'q':
+            if response == 'q': # quit when press "q"
                 break
             p1c = self.deck.rm_card()
             p2c = self.deck.rm_card()
@@ -119,6 +128,7 @@ class Game:
         print("War is over. '{}' wins".format(win))
 
     def winner(self, p1, p2):
+        # win the game
         if p1.wins > p2.wins:
             return p1.name
         if p1.wins < p2.wins:
