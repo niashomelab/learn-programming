9a70a3824f0d45f1b4fcc74fe89e3eb7a9cdba99
Nghia <niashomelab@gmail.com>
2018-08-24 13:13:46 +0200

    add more comment

--- python/chap15_wargame.py
+++ python/chap15_wargame.py
@@ -1,3 +1,4 @@
+# SOURCE: the self-taught Programmer; Cory Althoff 
 # WARGAME
 """
 Create the popular card game 'WAR'. In War, each player draws a card from the deck, and the player with the highest card wins. You will build War by defining classes representing a card, a deck, a player, and finally, the game itself.
@@ -26,18 +27,21 @@ class Card:
         self.suit = s
 
     def __lt__(self, c2):
-        # less than; card 2
+        # Magic Method "<" less than; card 2 = the 2nd card
         if self.value < c2.value:
+            # check for value (1st)
             return True
         if self.value == c2.value:
+            # when 2 cards have the same value
             if self.suit < c2.suit:
+                # then check for suit (2nd)
                 return True
             else:
                 return False
         return False
 
     def __gt__(self, c2):
-        # greater than; card 2
+        # Magic Method ">", greater than; card 2
         if self.value > c2.value:
             return True
         if self.value == c2.value:
@@ -48,7 +52,7 @@ class Card:
         return False
 
     def __repr__(self):
-        # print card i.e. 3 of diamonds
+        # format for p1c, p2c; print card i.e. 3 of diamonds
         v = self.values[self.value] \
             + " of " + \
             self.suits[self.suit]
@@ -57,18 +61,18 @@ class Card:
 # from random import shuffle for class Deck
 class Deck:
     def __init__(self):
-        # create 52 cards - one card for every suit+value combi; 52 = ((15-2)+1)*4
-        self.cards = [] # cards list
-        for i in range(2, 15): # 2..10 + jack + queen + king + ace = 13; last value = 14, begin with 2; 
+        # create a Deck: create 52 cards - one card for every suit+value combi; 52 = ((15-2)+1)*4
+        self.cards = [] # cards list = a deck
+        for i in range(2, 15): # 2..10 + jack + queen + king + ace = 13; last value = 14, begin with 2 
             for j in range(4): # 0,1,2,3 = 4 suits
                 self.cards.append(Card(i,j))
-        shuffle(self.cards) # randomly rearranges the items in the list "cards", mimickking the shuffling of a deck of cards
+        shuffle(self.cards) # randomly rearranges items in the list "cards", mimickking the shuffling of a deck of cards
 
     def rm_card(self):
-        # remove card from the list "cards"
-        if len(self.cards) == 0: # empty list => none
+        # remove card from the list "cards"; remove card from the deck
+        if len(self.cards) == 0: # empty list => none; no card in deck, remove nothing
             return
-        return self.cards.pop() # remove the drawed card
+        return self.cards.pop() # remove the drawn card from deck
 
 
 class Player:
@@ -82,7 +86,7 @@ class Game:
     def __init__(self):
         name1 = input("p1 name: ")
         name2 = input("p2 name: ")
-        self.deck = Deck() # create a new deck for 2 players
+        self.deck = Deck() # create a new deck for 2 players, total 52 cards, 26 cards each player
         self.p1 = Player(name1)
         self.p2 = Player(name2)
 
@@ -93,7 +97,7 @@ class Game:
         print(w)
 
     def draw(self, p1n, p1c, p2n, p2c):
-        d = "'{}' drew {} ; '{}' drew {}" # draw
+        d = "'{}' drew {} ; '{}' drew {}" # d = draw
         d = d.format(p1n, # player 1's name
                      p1c, # player 1's card
                      p2n, # player 2's name
@@ -101,22 +105,23 @@ class Game:
         print(d) # print draw list
 
     def play_game(self):
-        cards = self.deck.cards
+        cards = self.deck.cards # import cards from __init__ deck: self.deck = Deck()
         print("\nbeginning War!")
-        while len(cards) >= 2:
+        while len(cards) >= 2: # min 2 cards, each card for 1 person. Otherwise endgame
             m = "\nq to quit. Any key to play: " # m = message
             response = input(m)
             if response == 'q': # quit when press "q"
                 break
-            p1c = self.deck.rm_card() # remove drawned card from deck
+            p1c = self.deck.rm_card() # get a card from deck, remove drawn card from current deck
             p2c = self.deck.rm_card()
-            p1n = self.p1.name
+            p1n = self.p1.name # get player's name from class Player + class Game __init__
             p2n = self.p2.name
             self.draw(p1n,
                       p1c,
                       p2n,
                       p2c)
-            if p1c > p2c:
+            # print draw: "p1" drew Jack of HEARTS ; "p2" drew 5 of CLUBS
+            if p1c > p2c: # depend on Magic Method __lt__ and __gt__
                 self.p1.wins += 1
                 self.wins(self.p1.name)
             else:
@@ -125,15 +130,16 @@ class Game:
 
         win = self.winner(self.p1, self.p2)
         print("War is over. '{}' wins".format(win)) # Problem: War is over. 'It was a tie!' wins
-        # all of cards has been drawned; max = 52/2 = 26 rounds
+        
+        # all of cards has been drawn; max = 52/2 = 26 rounds
 
     def winner(self, p1, p2):
-        # win the game
+        # win the game; compare the numbers of won rounds
         if p1.wins > p2.wins:
             return p1.name
         if p1.wins < p2.wins:
             return p2.name
         return "It was a tie!"
 
-game = Game()
-game.play_game()
+game = Game() # create a newgame
+game.play_game() # play the game
